d291b187903f25c59dc3acba357898a21f51a873
Exxon <tomfagan03@gmail.com>
2021-03-15 05:19:26 +0000

    Forgot bracket oops
    Rebase 20/47 :)

--- src/Base/DefaultCommands/index.js
+++ src/Base/DefaultCommands/index.js
@@ -1,5 +1,4 @@
 module.exports = {
-<<<<<<< HEAD
   categories: require("./Categories"),
   commands: require("./Commands"),
   help: require("./Help"),
@@ -8,12 +7,3 @@ module.exports = {
   language: require("./Language"),
   ticketconfig: require("./TicketConfig"),
 };
-=======
-    categories: require("./Categories"),
-    commands: require("./Commands"),
-    help: require("./Help"),
-    requiredroles: require("./RequiredRoles"),
-    setprefix: require("./SetPrefix"),
-    ticketconfig: require("./TicketConfig")
-};
->>>>>>> 95f67e9 (Tickets)
