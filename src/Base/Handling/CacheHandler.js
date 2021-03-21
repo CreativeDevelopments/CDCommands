0d6f306758e3a27115b61e9329b18e3fa3cef8d5
Exxon <tomfagan03@gmail.com>
2021-03-21 19:14:13 +0000

    It won't let me change branch smh

--- src/Base/Handling/CacheHandler.js
+++ src/Base/Handling/CacheHandler.js
@@ -13,6 +13,7 @@ const disabledCommands = require("../../Database/models/disabled-commands");
  * }} T
  */
 module.exports = class Cache {
+
   /**
    * @private
    * @type {Collection<string, Collection<string, Document<any>>>}
@@ -20,7 +21,7 @@ module.exports = class Cache {
   _cache = new Collection();
   /**
    * @private
-   * @type {number}
+   * @type {number} 
    */
   _updateSpeed;
 
@@ -80,7 +81,7 @@ module.exports = class Cache {
 
   /**
    * @public
-   * @param {keyof T} type
+   * @param {keyof T} type 
    * @param {string} findBy
    */
   getDocument(type, findBy) {
@@ -90,19 +91,15 @@ module.exports = class Cache {
 
   /**
    * @public
-   * @param {keyof T} type
-   * @param {Document<any>} doc
+   * @param {keyof T} type  
+   * @param {Document<any>} doc 
    */
   insertDocument(type, doc) {
-    if (!this._cache.get(type))
-      this._cache.set(
-        type,
-        new Collection().set(doc[this._options.models[type].getBy], doc),
-      );
+    if (!this._cache.get(type)) this._cache.set(type, new Collection().set(doc[this._options.models[type].getBy], doc))
     else this._cache.get(type).set(doc[this._options.models[type].getBy], doc);
   }
 
-  /**
+  /** 
    * @public
    * @param {keyof T} type
    * @param {Document<any>} update
@@ -113,15 +110,15 @@ module.exports = class Cache {
 
   /**
    * @public
-   * @param {keyof T} type
-   * @param {string} findBy
+   * @param {keyof T} type 
+   * @param {string} findBy 
    */
   async deleteDocument(type, findBy) {
-    this._cache.get(type).delete(findBy);
+    this._cache.get(type).delete(findBy)
     const query = {};
     query[this._options.models[type].getBy] = findBy;
 
-    await this._models.get(type).findOneAndDelete(query);
+    await this._models.get(type).findOneAndDelete(query)
   }
 
   /** @private */
@@ -139,4 +136,4 @@ module.exports = class Cache {
       }
     }, this._updateSpeed);
   }
-};
+}
