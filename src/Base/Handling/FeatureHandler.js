9c889c78952227ce68dfebc4ba39382b2371d711
Maxisthemoose <oriontothemax@gmail.com>
2021-03-21 12:09:41 -0700

    seems done, just do readme and version update, i think its breaking but idk lol

---
+++ src/Base/Handling/FeatureHandler.js
@@ -0,0 +1,66 @@
+const { readdirSync } = require("fs");
+const { join } = require("path");
+const Feature = require("../Feature");
+
+module.exports = class FeatureHandler {
+  /**
+   * @private
+   * @type {import("../CDClient").CDClient}
+   */
+  _client;
+
+  /**
+   * @private
+   * @type {string}
+   */
+  _dir;
+  /**
+   * @param {import("../CDClient").CDClient} client
+   * @param {string=} dir
+   */
+  constructor(client, dir) {
+    this._client = client;
+    this._dir = dir;
+
+    const { path } = require.main;
+    if (dir && dir !== "") this._dir = join(path, dir);
+
+    try {
+      readdirSync(this._dir);
+    } catch (err) {
+      throw new ReferenceError('An invalid "features" path was provided.');
+    }
+
+    this._init();
+  }
+
+  /** @private */
+  _init() {
+    const files = readdirSync(this._dir);
+    for (const file of files) {
+      /** @type {Feature} */
+      const feature = require(join(this._dir, file));
+      if (!(feature instanceof Feature)) {
+        this._client.logError({
+          data: `Feature at path ${join(
+            this._dir,
+            file,
+          )} is not a valid feature. Please make sure it is set up correctly.`,
+        });
+        continue;
+      }
+
+      if (!feature.run || typeof feature.run !== "function") {
+        this._client.logError({
+          data: `Feature at path ${join(
+            this._dir,
+            file,
+          )} does not have a valid "run" function. Please make sure it is set up correctly.`,
+        });
+        continue;
+      }
+
+      feature.run(this._client);
+    }
+  }
+};
--- src/Base/Handling/MessageJSON.js
+++ src/Base/Handling/MessageJSON.js
@@ -6,7 +6,6 @@ const fs = require("fs");
  * @template {import("../message.json")} T
  */
 class MessageJSON {
-
   /**
    * @private
    * @type {string}
@@ -20,16 +19,17 @@ class MessageJSON {
   _fileData;
 
   /**
-   * @param {string=} messagePath 
+   * @param {string=} messagePath
    */
   constructor(messagePath) {
-    if (messagePath && messagePath !== "") this._path = join(require.main.path, messagePath);
+    if (messagePath && messagePath !== "")
+      this._path = join(require.main.path, messagePath);
     try {
-        require(this._path);
+      require(this._path);
     } catch (err) {
-      throw new ReferenceError("An invalid \"message.json\" path was provided.");
+      throw new ReferenceError('An invalid "message.json" path was provided.');
     }
-    
+
     this._fileData = require(this._path);
   }
   /**
@@ -38,15 +38,14 @@ class MessageJSON {
    * @param {V} key
    * @param {S} secondary_key
    * @param {[{ key: keyof import("../json-schema/replacers.json"), replace: string }]} args
-   * @returns {import("../message.json")[V] extends keyof string ? import("../message.json")[V] : import("../message.json")[V][S]} 
+   * @returns {import("../message.json")[V] extends keyof string ? import("../message.json")[V] : import("../message.json")[V][S]}
    */
   getValue(key, secondary_key, args) {
     let value = this._fileData[key];
-    if (typeof value === "object")
-      value = value[secondary_key];
+    if (typeof value === "object") value = value[secondary_key];
 
     for (const replacer of args) {
-      const regex = new RegExp(`{${replacer.key}}`, "g")
+      const regex = new RegExp(`{${replacer.key}}`, "g");
       value = value.replace(regex, replacer.replace);
     }
 
@@ -56,16 +55,16 @@ class MessageJSON {
   /**
    * Writes a new value to the specified key value in the "message.json" file, if the key already exists.
    * @template {string} K
-   * @param {string} key 
-   * @param {K} value 
+   * @param {string} key
+   * @param {K} value
    * @returns {K}
    */
   setValue(key, value) {
-    if (!this._fileData[key]) throw new ReferenceError(`Unknown key: \"${key}\"`);
+    if (!this._fileData[key])
+      throw new ReferenceError(`Unknown key: \"${key}\"`);
     this._fileData[key] = value;
     fs.writeFileSync(this._path, this._fileData);
     return value;
   }
-
 }
 module.exports = MessageJSON;
