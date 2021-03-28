93edc9c60e3901bb9ef56e91f142caac27de149f
Maxisthemoose <oriontothemax@gmail.com>
2021-03-28 11:45:02 -0700

    Changed how replacers work for embeds, made them work the same as regular string, ie, array of replacers

--- src/Base/Handling/MessageJSON.js
+++ src/Base/Handling/MessageJSON.js
@@ -46,66 +46,66 @@ class MessageJSON {
    * [{ key: keyof import("../json-schema/replacers.json"), replace: string }]
    * :
    * {
-   *  title?: {
+   *  title?: [{
    *   key: keyof import("../json-schema/replacers.json")["EMBED"] | keyof import("../json-schema/replacers.json")
    *   replace: string
-   * };
-   * url?: {
+   * }];
+   * url?: [{
    *   key: keyof import("../json-schema/replacers.json")["EMBED"] | keyof import("../json-schema/replacers.json")
    *   replace: string
-   * };
-   * author_name?: {
+   * }];
+   * author_name?: [{
    *   key: keyof import("../json-schema/replacers.json")["EMBED"] | keyof import("../json-schema/replacers.json")
    *   replace: string
-   * }
-   * author_iconURL?: {
+   * }]
+   * author_iconURL?: [{
    *   key: keyof import("../json-schema/replacers.json")["EMBED"] | keyof import("../json-schema/replacers.json")
    *   replace: string
-   * }
-   * color?: {
+   * }]
+   * color?: [{
    *   key: keyof import("../json-schema/replacers.json")["EMBED"] | keyof import("../json-schema/replacers.json")
    *   replace: string
-   * }
+   * }]
    * fields?: [
    *  {
-   *    name?: {
+   *    name?: [{
    *      key: keyof import("../json-schema/replacers.json")["EMBED"] | keyof import("../json-schema/replacers.json")
    *      replace: string
-   *    }
-   *    value?: {
+   *    }]
+   *    value?: [{
    *      key: keyof import("../json-schema/replacers.json")["EMBED"] | keyof import("../json-schema/replacers.json")
    *      replace: string
-   *    }
-   *    inline?: {
-   *      key: keyof import("../json-schema/replacers.json")["EMBED"] | keyof import("../json-schema/replacers.json")
-   *      replace: string
-   *    }
+   *    }]
+   *    inline?: [{
+   *   key: keyof import("../json-schema/replacers.json")["EMBED"] | keyof import("../json-schema/replacers.json")
+   *   replace: string
+   *  }]
    *  }
    * ]
-   * footer_text?: {
+   * footer_text?: [{
    *   key: keyof import("../json-schema/replacers.json")["EMBED"] | keyof import("../json-schema/replacers.json")
    *   replace: string
-   * }
-   * footer_iconURL?: {
+   * }]
+   * footer_iconURL?: [{
    *   key: keyof import("../json-schema/replacers.json")["EMBED"] | keyof import("../json-schema/replacers.json")
    *   replace: string
-   * }
-   * timestamp?: {
+   * }]
+   * timestamp?: [{
    *   key: keyof import("../json-schema/replacers.json")["EMBED"] | keyof import("../json-schema/replacers.json")
    *   replace: string
-   * }
-   * thumbnail_url?: {
+   * }]
+   * thumbnail_url?: [{
    *   key: keyof import("../json-schema/replacers.json")["EMBED"] | keyof import("../json-schema/replacers.json")
    *   replace: string
-   * }
-   * description?: {
+   * }]
+   * description?: [{
    *   key: keyof import("../json-schema/replacers.json")["EMBED"] | keyof import("../json-schema/replacers.json")
    *   replace: string
-   * }
-   * image_url?: {
+   * }]
+   * image_url?: [{
    *   key: keyof import("../json-schema/replacers.json")["EMBED"] | keyof import("../json-schema/replacers.json")
    *   replace: string
-   * }
+   * }]
    * }} args
    * @returns {is extends "string" ? string : MessageEmbed}
    */
@@ -130,6 +130,11 @@ class MessageJSON {
 
       return get;
     } else if (is === "embed") {
+      if (args instanceof Array)
+        throw new Error(
+          'Got "is" as "embed" but "args" is an instance of an Array',
+        );
+
       for (const args_key of Object.keys(args)) {
         if (args_key === "fields") {
           const fields_keys = args[args_key];
@@ -139,58 +144,125 @@ class MessageJSON {
 
             const { inline, value, name } = args_keys_inline_etc;
 
-            if (inline) {
-              const inlineRegex = new RegExp(`{${inline.key}}`, "g");
-              get_field["inline"] = get_field["inline"].replace(
-                inlineRegex,
-                inline.replace,
+            if (inline instanceof Array) {
+              for (let i = 0; i < inline.length; i++) {
+                const key_value = inline[i];
+                if (!key_value.key) {
+                  console.log(
+                    "[WARN] ".yellow +
+                      `Argument value at position "${i}" in "${args_key}->inline" is missing a valid key value, skipping replacer.`,
+                  );
+                  continue;
+                }
+                if (!key_value.replace) {
+                  console.log(
+                    "[WARN] ".yellow +
+                      `Argument value at position "${i}" in "${args_key}->inline" is missing a valid replace value, skipping replacer.`,
+                  );
+                  continue;
+                }
+                const inlineRegex = new RegExp(`{${key_value.key}}`, "g");
+                get_field["inline"] = get_field["inline"].replace(
+                  inlineRegex,
+                  key_value.replace,
+                );
+              }
+            } else if (!(inline instanceof Array) && inline !== undefined)
+              console.log(
+                "[WARN] ".yellow +
+                  `Got "inline" value in "fields" at position "${i}" as non-Array.`,
               );
-            }
-            if (value) {
-              const valueRegex = new RegExp(`{${value.key}}`, "g");
-              get_field["value"] = get_field["value"].replace(
-                valueRegex,
-                value.replace,
+
+            if (value instanceof Array) {
+              for (let i = 0; i < value.length; i++) {
+                const key_value = value[i];
+                if (!key_value.key) {
+                  console.log(
+                    "[WARN] ".yellow +
+                      `Argument value at position "${i}" in "${args_key}->value" is missing a valid key value, skipping replacer.`,
+                  );
+                  continue;
+                }
+                if (!key_value.replace) {
+                  console.log(
+                    "[WARN] ".yellow +
+                      `Argument value at position "${i}" in "${args_key}->value" is missing a valid replace value, skipping replacer.`,
+                  );
+                  continue;
+                }
+                const valueRegex = new RegExp(`{${key_value.key}}`, "g");
+                get_field["value"] = get_field["value"].replace(
+                  valueRegex,
+                  key_value.replace,
+                );
+              }
+            } else if (!(value instanceof Array) && value !== undefined)
+              console.log(
+                "[WARN] ".yellow +
+                  `Got "value" value in "fields" at position "${i}" as non-Array.`,
               );
-            }
-            if (name) {
-              const nameRegex = new RegExp(`{${name.key}}`, "g");
-              get_field["name"] = get_field["name"].replace(
-                nameRegex,
-                name.replace,
+            if (name instanceof Array) {
+              for (let i = 0; i < name.length; i++) {
+                const key_value = name[i];
+                if (!key_value.key) {
+                  console.log(
+                    "[WARN] ".yellow +
+                      `Argument value at position "${i}" in "${args_key}->name" is missing a valid key value, skipping replacer.`,
+                  );
+                  continue;
+                }
+                if (!key_value.replace) {
+                  console.log(
+                    "[WARN] ".yellow +
+                      `Argument value at position "${i}" in "${args_key}->name" is missing a valid replace value, skipping replacer.`,
+                  );
+                  continue;
+                }
+                const nameRegex = new RegExp(`{${key_value.key}}`, "g");
+                get_field["name"] = get_field["name"].replace(
+                  nameRegex,
+                  key_value.replace,
+                );
+              }
+            } else if (!(name instanceof Array) && name !== undefined)
+              console.log(
+                "[WARN] ".yellow +
+                  `Got "name" value in "fields" at position "${i}" as non-Array.`,
               );
-            }
           }
           continue;
         }
 
-        if (!args[args_key].key) {
-          console.log(
-            "[WARN]".yellow +
-              ` Argument value "${args_key}" is missing a valid key value, skipping replacer.`,
-          );
-          continue;
-        } else if (!args[args_key].replace) {
-          console.log(
-            "[WARN]".yellow +
-              ` Argument value "${args_key}" is missing a valid replace value, skipping replacer.`,
-          );
-          continue;
-        }
-        if (args_key.includes("_")) {
-          const _keys = args_key.split("_");
-          const regex = new RegExp(`{${args[args_key].key}}`, "g");
+        for (let i = 0; i < args[args_key].length; i++) {
+          const key_value = args[args_key][i];
+          if (!key_value.key) {
+            console.log(
+              "[WARN] ".yellow +
+                `Argument value at position "${i}" in "${args_key}" is missing a valid key value, skipping replacer.`,
+            );
+            continue;
+          } else if (!key_value.replace) {
+            console.log(
+              "[WARN] ".yellow +
+                `Argument value at position "${i}" in "${args_key}" is missing a valid replace value, skipping replacer.`,
+            );
+            continue;
+          }
+          if (args_key.includes("_")) {
+            const _keys = args_key.split("_");
+            const regex = new RegExp(`{${key_value.key}}`, "g");
 
-          get["embed"][_keys[0]][_keys[1]] = get["embed"][_keys[0]][
-            _keys[1]
-          ].replace(regex, args[args_key].replace);
-          continue;
-        } else {
-          const regex = new RegExp(`{${args[args_key].key}}`, "g");
-          get["embed"][args_key] = get["embed"][args_key].replace(
-            regex,
-            args[args_key].replace,
-          );
+            get["embed"][_keys[0]][_keys[1]] = get["embed"][_keys[0]][
+              _keys[1]
+            ].replace(regex, key_value.replace);
+            continue;
+          } else {
+            const regex = new RegExp(`{${key_value.key}}`, "g");
+            get["embed"][args_key] = get["embed"][args_key].replace(
+              regex,
+              key_value.replace,
+            );
+          }
         }
       }
       return new MessageEmbed(get.embed);
@@ -200,5 +272,19 @@ class MessageJSON {
       );
   }
 }
+
+// new MessageJSON("").getValue(
+//   "en",
+//   "GUILD_ONLY",
+//   "",
+//   { is: "embed" },
+//   {
+//     description: [
+//       {
+//         key,
+//       },
+//     ],
+//   },
+// );
 
 module.exports = MessageJSON;
