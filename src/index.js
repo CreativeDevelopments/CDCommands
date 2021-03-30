961f4ba84263652a453eec197741d0279c26ac75
Maxisthemoose <oriontothemax@gmail.com>
2021-03-30 11:01:15 -0700

    hm

--- src/index.js
+++ src/index.js
@@ -89,6 +89,7 @@ class CDCommands {
    * @param {{
    * commandsDir?: string;
    * eventsDir?: string;
+   * featuresDir?: string;
    * testServers?: string[];
    * customMessageEvent?: boolean;
    * customHelpCommand?: boolean;
@@ -125,9 +126,9 @@ class CDCommands {
           "file!",
       );
     } catch (err) {}
-
     if (!options.commandsDir) options.commandsDir = "commands";
     if (!options.eventsDir) options.eventsDir = "events";
+    if (!options.featuresDir) options.featuresDir = "features";
     if (!options.testServers) options.testServers = [];
     if (!options.devs) options.devs = [];
     if (!options.MessageJSONPath) options.MessageJSONPath = "";
@@ -135,6 +136,7 @@ class CDCommands {
     this._client = client;
     this._commandsDir = options.commandsDir;
     this._eventsDir = options.eventsDir;
+    this._featuresDir = options.featuresDir;
     this._testServers = options.testServers;
     this._defaultPrefix = options.defaultPrefix;
     this._mongoURI = options.mongoURI;
